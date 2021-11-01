c934963e43e1608603548846397b734f16db8e60
karthikVIT <93507074+karthikVIT@users.noreply.github.com>
2021-11-01 22:17:59 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -5,7 +5,7 @@ face_detection = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 camera = cv2.VideoCapture(0)
 
 user_name = input("Enter the Name: ")
-count = 0
+num = 0
 
 while(True):
     ret, color_img = camera.read()
@@ -13,16 +13,16 @@ while(True):
     faces = face_detection.detectMultiScale(gray_image, 1.3, 5)
 
     for (x, y, w, h) in faces:
-        count += 1
+        num += 1
         cv2.imwrite("DataSet/User."+str(user_name)+"." +
-                    str(count)+".jpg", gray_image[y:y+h, x:x+w])
+                    str(num)+".jpg", gray_image[y:y+h, x:x+w])
         cv2.rectangle(color_img, (x, y), (x+w, y+h), (0, 0, 255), 2)
         cv2.waitKey(100)
 
     cv2.imshow("Face Detection", color_img)
     cv2.waitKey(1)
 
-    if(count > 15):
+    if(num > 15):
         break
 
 camera.release()
